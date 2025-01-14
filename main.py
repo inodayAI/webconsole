583113cb127aa61fe20575cc5f188ecf2c8c4981
inodayAI <inodayai2007@gmail.com>
2025-01-14 18:25:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8900)
+    port = int(os.environ.get('PORT', 8080))  # Default port is 8080
+    app.run(host='0.0.0.0', port=port)
